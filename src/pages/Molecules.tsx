b2623b5ec9543094e8e3933f36bf0ad8def59fd1
Simon Norberg <simon@pthread.se>
2025-02-09 20:23:44 +0100

    Simple table of all molecules

--- src/pages/Molecules.tsx
+++ src/pages/Molecules.tsx
@@ -1,6 +1,72 @@
-const Molecules = () => {
-    return <h2>Molecules Page</h2>;
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import "../App.css";
+
+interface Molecule {
+  formula: string;
+  property: {
+    Name?: string;
+    Description?: string;
+    DescriptionAttribution?: string;
+    DescriptionLicense?: string;
   };
-  
-  export default Molecules;
-  
+}
+
+const fetchMolecules = async (): Promise<Molecule[]> => {
+  const { data } = await axios.get("http://localhost:8000/molecule");
+  return data;
+};
+
+const Molecules = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["molecules"],
+    queryFn: fetchMolecules,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error when fetching data.</p>;
+
+  return (
+    <div className="container">
+      <h2>Molecules</h2>
+      <table className="molecule-table">
+        <thead>
+          <tr>
+            <th>Formula</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((molecule, index) => (
+            <tr key={index}>
+              <td>{molecule.formula}</td>
+              <td>{molecule.property.Name || "N/A"}</td>
+              <td>
+                {molecule.property.Description ? (
+                  <>
+                    {molecule.property.Description}{" "}
+                    {molecule.property.DescriptionAttribution && (
+                      <a
+                        href={molecule.property.DescriptionAttribution}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        [Source]
+                      </a>
+                    )}
+                  </>
+                ) : (
+                  "N/A"
+                )}
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Molecules;
